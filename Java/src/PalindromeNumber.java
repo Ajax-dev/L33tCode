8ffc1f581b85312b49e052cf2a9ba3484da8f54b
Ajax-dev <ajwilt97@gmail.com>
2022-01-10 18:24:01 +0000

    Uploaded my Palindrome Solution for leetcode problem 9, testing out JUnit

---
+++ Java/src/PalindromeNumber.java
@@ -0,0 +1,20 @@
+class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        String str1 = String.valueOf(x);
+        char[] input = new char[str1.length()];
+        int counter = 0;
+        for (int i = (str1.length() - 1); i >= 0; i--){
+            input[counter] = str1.charAt(i);
+            counter++;
+        }
+        String str2 = new String(input);
+        if (str1.equals(str2)) {
+            System.out.println("TRUE| " + str1 + " is a Palindrome as reversed is: " + str2);
+            return true;
+        } else {
+            System.out.println("FALSE| " + str1 + " reversed is: " + str2);
+            return false;
+        }
+    }
+}
+
---
+++ Java/src/PalindromeTest.java
@@ -0,0 +1,31 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PalindromeTest {
+    PalindromeNumber pal;
+
+    @BeforeEach
+    void setUp() {
+        pal = new PalindromeNumber();
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Easy assertion of 1==1")
+    void oneIsOne() {
+        Assert.assertEquals(1, 1);
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Testing a variety of palindromes and non-palindromes")
+    void isPalindrome() {
+        Assert.assertTrue(pal.isPalindrome(121));
+        Assert.assertFalse(pal.isPalindrome(122));
+        Assert.assertTrue(pal.isPalindrome(122222221));
+        Assert.assertTrue(pal.isPalindrome(1011001101));
+        Assert.assertFalse(pal.isPalindrome(1223123));
+    }
+
+}
